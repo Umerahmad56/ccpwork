dd29501fba23052b294ea106ffca974ee5763b91
Muhammad Umer <mainumerahmad.@gmail.com>
2026-02-11 19:55:14 +0000

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from DevSecOps Workshop!');
+});
+
+// Changed from localhost to 0.0.0.0
+app.listen(port, '0.0.0.0', () => {
+  console.log('App listening at http://0.0.0.0:' + port);
+});
